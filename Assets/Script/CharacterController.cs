97f1eef8b781d81d08638820d17abb4860677c78
Fares Oulmane <6-15352@saeinstitute.edu>
2021-01-25 18:33:50 +0100

    add cube

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -7,12 +7,12 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hiii");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hello");
+        Debug.Log("add cube");
     }
 }
