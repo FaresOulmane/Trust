2e85ed51268edec7069d1bb30fb254fec5f7f941
Fares Oulmane <6-15352@saeinstitute.edu>
2021-01-25 17:52:31 +0100

    test bis

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -7,7 +7,7 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hi");
+        Debug.Log("hiii");
     }
 
     // Update is called once per frame
