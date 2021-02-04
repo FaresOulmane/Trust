a8ec79b1e61a67cf25d428ef5414bddfea57dd96
Fares Oulmane <6-15352@saeinstitute.edu>
2021-02-04 16:04:42 +0100

    character controller

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("add cube");
-    }
+    private float speed;
+      [SerializeField] private float runSpeed;
+      [SerializeField] private float walkSpeed;
+      UnityEngine.CharacterController cc;
+     
+      [SerializeField] private float timeToRun;
+   
+       private float turnSmoothVelocity;
+      [SerializeField] private float turnSmoothTime;
+      [SerializeField] private Transform cam;
+      private float playerYVelocity;
+    
+       private void Awake()
+       { 
+           speed = walkSpeed;
+           cc = GetComponent<UnityEngine.CharacterController>();
+       }
+   
+       private void Update()
+       {
+          PlayerMove();
+          
+       }
+   
+       void PlayerMove()
+       {
+           Vector3 moveVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+           playerYVelocity += Physics.gravity.y * Time.deltaTime;
+   
+           if (playerYVelocity < 0)
+           {
+               playerYVelocity = -1;
+           }
+           if (moveVector.magnitude >= 0.1f)
+           {
+               float targetAngle = Mathf.Atan2(moveVector.x, moveVector.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+               float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
+                   turnSmoothTime);
+               
+               transform.rotation = Quaternion.Euler(0,angle,0);
+              
+               Vector3 moveDirection =  Quaternion.Euler(0,targetAngle,0) * Vector3.forward;
+               moveDirection.y = playerYVelocity;
+              cc.Move(moveDirection.normalized * (speed * Time.deltaTime));
+             
+           }
+           if (Input.GetKey(KeyCode.LeftShift))
+               speed = Mathf.Lerp(speed, runSpeed, Time.deltaTime * timeToRun);
+           else
+               speed = Mathf.Lerp(speed, walkSpeed, Time.deltaTime * timeToRun);
+           
+       }
 }
